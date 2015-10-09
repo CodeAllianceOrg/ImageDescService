9dcd446a123726c2ec672587132e1cd79e7bb1eb
John Brugge <john.brugge@gmail.com>
2015-10-09 12:27:09 -0500

    MMLC-880: Testing out original setting on staging.

--- config/application.rb
+++ config/application.rb
@@ -42,8 +42,8 @@ module DiagramRailsApp
     config.filter_parameters += [:password]
 
     config.assets.enabled = false
-    # Avoid database connection during app initialization
-    config.assets.initialize_on_precompile = false
+    # Config whether app gets inialized, which includes database connection, during app initialization
+    config.assets.initialize_on_precompile = true
   end
   
   require 'sass'
